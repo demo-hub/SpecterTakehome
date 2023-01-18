2875bc2692fc044b95a887946bff44e0ee13b3e5
Joao <joaop86@hotmail.com>
2023-01-18 16:37:52 +0000

    first commit

---
+++ app/src/api/companies.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { Company } from "src/types";
+
+import data from "./companies.json";
+
+export function useCompanies() {
+  return useQuery(["companyData"], () => Promise.resolve(data as Company[]));
+}
