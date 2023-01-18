186c1701688e328c5939f1c50c0d6b968feeed77
Joao <joaop86@hotmail.com>
2023-01-18 19:57:37 +0000

    fix absolute paths

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,13 @@
-import path from "path";
-
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [{ find: "src", replacement: path.resolve(__dirname, "src") }],
-  },
   plugins: [
     react(),
+    tsconfigPaths(),
     checker({
       typescript: true,
       eslint: {
