5833a24f670b88f610c4434334f4072790ff7c63
Joao <joaop86@hotmail.com>
2023-01-23 19:24:07 +0000

    details page v1

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,19 +3,34 @@ import { useIndustries } from "@api/industries";
 import { useRegions } from "@api/regions";
 import {
   Box,
+  Card,
+  CardBody,
   Drawer,
   DrawerBody,
   DrawerCloseButton,
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
-  Input,
+  Flex,
   SimpleGrid,
   Spinner,
+  Stack,
+  Stat,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tag,
+  Text,
   useDisclosure,
 } from "@chakra-ui/react";
 import CompanyCard from "@components/companyCard";
 import FilterBar from "@components/filterBar";
+import formatNumber from "@utils/formatNumber";
 import { useEffect, useState } from "react";
 import { Company } from "types";
 
@@ -166,16 +181,92 @@ function App() {
               )
             : undefined}
           {isFetchingNextPage ? <Spinner color="purple" /> : undefined}
-          <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="lg">
+          <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="xl">
             <DrawerOverlay />
             <DrawerContent>
               <DrawerCloseButton />
               <DrawerHeader>
                 #{selectedCompany?.Rank} {selectedCompany?.["Company Name"]}
+                <Tag ml="1" fontSize="0.8em" colorScheme="purple">
+                  {selectedCompany?.Industry}
+                </Tag>
               </DrawerHeader>
 
               <DrawerBody>
-                <Input placeholder="Type here..." />
+                <Tabs colorScheme="purple">
+                  <TabList>
+                    <Tab>General Info</Tab>
+                    <Tab>Growth</Tab>
+                  </TabList>
+
+                  <TabPanels>
+                    <TabPanel>
+                      <Stack spacing="2">
+                        <Text fontSize="lg" as="b">
+                          Summary
+                        </Text>
+                        <Text>{selectedCompany?.Description}</Text>
+                        <Text>
+                          Founded by {selectedCompany?.Founders} in{" "}
+                          {selectedCompany?.["Founded Date"]}
+                        </Text>
+                      </Stack>
+                      <Stack spacing="2" mt="8">
+                        <Text fontSize="lg" as="b">
+                          Funding
+                        </Text>
+                        <Flex>
+                          <Card>
+                            <CardBody>
+                              <Stat>
+                                <StatLabel>Total Funding</StatLabel>
+                                <StatNumber>
+                                  {formatNumber(
+                                    selectedCompany?.["Total Funding Amount (in USD)"] ?? 0
+                                  ) ?? "-"}
+                                  $
+                                </StatNumber>
+                                <StatHelpText>
+                                  {selectedCompany?.["Last Funding Type"]}
+                                </StatHelpText>
+                              </Stat>
+                            </CardBody>
+                          </Card>
+                          <Card ml="4">
+                            <CardBody>
+                              <Stat>
+                                <StatLabel>Last Funding</StatLabel>
+                                <StatNumber>
+                                  {formatNumber(
+                                    selectedCompany?.["Last Funding Amount (in USD)"] ?? 0
+                                  ) ?? "-"}
+                                  $
+                                </StatNumber>
+                                <StatHelpText>
+                                  {selectedCompany?.["Last Funding Date"]}
+                                </StatHelpText>
+                              </Stat>
+                            </CardBody>
+                          </Card>
+                          <Card ml="4">
+                            <CardBody>
+                              <Stat>
+                                <StatLabel>Investors</StatLabel>
+                                <StatNumber>{selectedCompany?.["Number of Investors"]}</StatNumber>
+                                <StatHelpText>
+                                  On {selectedCompany?.["Number of Funding Rounds"]} rounds
+                                </StatHelpText>
+                              </Stat>
+                            </CardBody>
+                          </Card>
+                        </Flex>
+                      </Stack>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>two!</p>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
               </DrawerBody>
             </DrawerContent>
           </Drawer>
